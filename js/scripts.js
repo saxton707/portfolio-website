18f90af14c953766d59120c2f1cca522196b9356
saxton707 <70099488+saxton707@users.noreply.github.com>
2020-09-13 13:06:12 -0400

    Addign hamburger menu to mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
 console.log("Hello World");
+
+/*function mobileMenu() {
+  var x = document.getElementById("toggleNav");
+  if (yes.clicked === true) {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} */
+
+function myFunction() {
+	var x = document.getElementById("myNavToggle");
+	if (x.className === "navtoggle") {
+		x.className += "responsive";
+	}
+	else {
+		x.className = "navtoggle";
+	}
+}
